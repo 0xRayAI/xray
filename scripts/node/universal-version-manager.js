f349dce074ec8f20cbc1f3973cc62100ce4218e6
htafolla <htafolla@gmail.com>
2026-03-30 15:46:42 -0500

    chore: bump UVM to 1.15.35

--- scripts/node/universal-version-manager.js
+++ scripts/node/universal-version-manager.js
@@ -78,8 +78,8 @@ const CALCULATED_COUNTS = calculateCounts();
 const OFFICIAL_VERSIONS = {
   // Framework version
   framework: {
-    version: "1.15.34",
-    displayName: "StringRay AI v1.15.34",
+    version: "1.15.35",
+    displayName: "StringRay AI v1.15.35",
     lastUpdated: "2026-03-30",
     // Counts (auto-calculated, but can be overridden)
     ...CALCULATED_COUNTS,
